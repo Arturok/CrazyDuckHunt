2bba5142a5e9531f9b2a810db48406f036ef5f88
Arturok <mchinchilla11@gmail.com>
2015-07-28 14:03:52 -0600

    Primer commit

---
+++ XmlParser.cpp
@@ -0,0 +1,18 @@
+/*
+ * XmlParser.cpp
+ *
+ *  Created on: 28 de jul. de 2015
+ *      Author: arturo
+ */
+
+#include "XmlParser.h"
+
+XmlParser::XmlParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+XmlParser::~XmlParser() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ XmlParser.h
@@ -0,0 +1,17 @@
+/*
+ * XmlParser.h
+ *
+ *  Created on: 28 de jul. de 2015
+ *      Author: arturo
+ */
+
+#ifndef XMLPARSER_H_
+#define XMLPARSER_H_
+
+class XmlParser {
+public:
+	XmlParser();
+	virtual ~XmlParser();
+};
+
+#endif /* XMLPARSER_H_ */
